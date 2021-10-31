a1910745b97e966d39e97c885a1b67d38574f84a
Wataru Kanzaki <kanzwataru@gmail.com>
2021-10-31 01:30:47 -0400

    vk_hello: Initialize instance, physical device, queue family

--- vk_hello/main.c
+++ vk_hello/main.c
@@ -11,9 +11,11 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
-#define VK_CHECK(x) \
+#define countof(x) (sizeof(x) / sizeof(x[0]))
+
+#define VK_CHECK(x_) \
 	do {\
-		VkResult err = x;\
+		VkResult err = x_;\
 		if(err != VK_SUCCESS) {\
 			char buf[128];\
 			snprintf(buf, sizeof(buf), "Vulkan error %d at line %d\n", err, __LINE__);\
@@ -24,27 +26,101 @@
 
 struct VK {
 	VkInstance instance;
+	VkPhysicalDevice physical_device;
+
+	uint32_t queue_graphics_idx;
 };
 
 static struct VK s_vk;
 static SDL_Window *s_window;
 
-static void vk_init(struct VK *vk)
+#define CHECK(x_, msg_) do { if(!x_) { panic(msg_); } } while(0);
+static void panic(const char *message)
 {
+	fprintf(stderr, "%s\n", message);
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Critical Error", message, NULL);
+	abort();
+}
+
+static void vk_init_instance(struct VK *vk)
+{
+	/* extensions */
 	const char *extension_names[256];
-	unsigned int extension_count;
+	uint32_t extension_count = countof(extension_names);
 
 	SDL_Vulkan_GetInstanceExtensions(s_window, &extension_count, extension_names);
 
+	/* layers */
+	VkLayerProperties available_layers[256];
+	uint32_t available_layer_count = countof(available_layers);
+
+	vkEnumerateInstanceLayerProperties(&available_layer_count, available_layers);
+
+	const char *layer_names[] = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+	uint32_t layer_count = countof(layer_names);
+
+	static_assert(countof(layer_names) == 1, "Edit the code below to support more");
+	bool found = false;
+	for(uint32_t i = 0; i < available_layer_count; ++i) {
+		if(0 == strcmp(available_layers[i].layerName, layer_names[0])) {
+			found = true;
+			break;
+		}
+	}
+
+	if(!found) {
+		layer_count = 0;
+	}
+
+	/* instance */
 	VkInstanceCreateInfo instance_create_info = {
 		.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-		//.enabledLayerCount = extension_count,
-		//.ppEnabledLayerNames = extension_names
+		.enabledLayerCount = layer_count,
+		.ppEnabledLayerNames = layer_names,
 		.enabledExtensionCount = extension_count,
 		.ppEnabledExtensionNames = extension_names
 	};
 
 	VK_CHECK(vkCreateInstance(&instance_create_info, NULL, &vk->instance));
+}
+
+static void vk_init_physical_device(struct VK *vk)
+{
+	VkPhysicalDevice devices[256];
+	uint32_t device_count = countof(devices);
+
+	vkEnumeratePhysicalDevices(vk->instance, &device_count, devices);
+	CHECK(device_count, "No GPUs found");
+
+	// TODO: Favour discrete GPU over integrated
+	vk->physical_device = devices[0];
+}
+
+static void vk_init_queues(struct VK *vk)
+{
+	VkQueueFamilyProperties queue_families[32];
+	uint32_t queue_families_count = countof(queue_families);
+	vkGetPhysicalDeviceQueueFamilyProperties(vk->physical_device, &queue_families_count, queue_families);
+
+	bool found = false;
+	for(uint32_t i = 0; i < queue_families_count; ++i) {
+		if(queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			found = true;
+			vk->queue_graphics_idx = i;
+			break;
+		}
+	}
+
+	CHECK(found, "No graphics queue found");
+}
+
+static void vk_init(struct VK *vk)
+{
+	vk_init_instance(vk);
+	vk_init_physical_device(vk);
+	vk_init_queues(vk);
 }
 
 static void vk_destroy(struct VK *vk)
